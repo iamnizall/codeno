20e8864e3debc706df2ac8c183da2d657f889679
iamnizall <nizalrizqi69@gmail.com>
2021-12-11 05:12:32 +0700

    fix print

--- resources/views/finance/print.blade.php
+++ resources/views/finance/print.blade.php
@@ -115,9 +115,6 @@
               <div class="row no-print">
                 <div class="col-12">
                   <a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-                  <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-                    Payment
-                  </button>
                   <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
                     <i class="fas fa-download"></i> Generate PDF
                   </button>
@@ -125,9 +122,9 @@
               </div>
             </div>
             <!-- /.invoice -->
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+          </div>
+        </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
