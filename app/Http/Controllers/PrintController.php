c8a0135410d83099b8e6a68684b6b6512a8c5e99
iamnizall <nizalrizqi69@gmail.com>
2021-12-26 22:59:16 +0700

    remove controller

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -14,58 +14,6 @@ class PrintController extends Controller
         $invc = Invoice::all();
         return view('/finance/print', compact('invc'));
     }
-    public function printlocal()
-    {
-        // return view('finance/printlocal');
-        $html = view('/finance/printlocal');
-
-        // instantiate and use the dompdf class
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml($html);
-
-        // (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'portrait');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-        // Output the generated PDF to Browser
-        $dompdf->stream();
-    }
-    public function printluar()
-    {
-        $html = view('/finance/printluar');
-
-        // instantiate and use the dompdf class
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml($html);
-
-        // (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'portrait');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-        // Output the generated PDF to Browser
-        $dompdf->stream();
-    }
-    public function printsql()
-    {
-        $html = view('/finance/printsql');
-
-        // instantiate and use the dompdf class
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml($html);
-
-        // (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'portrait');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-        // Output the generated PDF to Browser
-        $dompdf->stream();
-    }
     public function team()
     {
         return view('/finance/team');
