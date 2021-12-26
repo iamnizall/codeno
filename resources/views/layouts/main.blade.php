8e9d8ef7c6f04514edc0548bd83a6b0e34927b01
iamnizall <nizalrizqi69@gmail.com>
2021-12-26 21:07:51 +0700

    delete resource

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -93,88 +93,6 @@
                     </li>
 
                     <!-- Messages Dropdown Menu -->
-                    <li class="nav-item dropdown">
-                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                            <a href="#" class="dropdown-item">
-                                <!-- Message Start -->
-                                <div class="media">
-                                    <img src="{{ asset('') }}assets/dist/img/user1-128x128.jpg" alt="User Avatar"
-                                    class="img-size-50 mr-3 img-circle">
-                                    <div class="media-body">
-                                        <h3 class="dropdown-item-title">
-                                            Brad Diesel
-                                            <span class="float-right text-sm text-danger"><i
-                                                class="fas fa-star"></i></span>
-                                            </h3>
-                                            <p class="text-sm">Call me whenever you can...</p>
-                                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                        </div>
-                                    </div>
-                                    <!-- Message End -->
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a href="#" class="dropdown-item">
-                                    <!-- Message Start -->
-                                    <div class="media">
-                                        <img src="{{ asset('') }}assets/dist/img/user8-128x128.jpg" alt="User Avatar"
-                                        class="img-size-50 img-circle mr-3">
-                                        <div class="media-body">
-                                            <h3 class="dropdown-item-title">
-                                                John Pierce
-                                                <span class="float-right text-sm text-muted"><i
-                                                    class="fas fa-star"></i></span>
-                                                </h3>
-                                                <p class="text-sm">I got your message bro</p>
-                                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                            </div>
-                                        </div>
-                                        <!-- Message End -->
-                                    </a>
-                                    <div class="dropdown-divider"></div>
-                                    <a href="#" class="dropdown-item">
-                                        <!-- Message Start -->
-                                        <div class="media">
-                                            <img src="{{ asset('') }}assets/dist/img/user3-128x128.jpg" alt="User Avatar"
-                                            class="img-size-50 img-circle mr-3">
-                                            <div class="media-body">
-                                                <h3 class="dropdown-item-title">
-                                                    Nora Silvester
-                                                    <span class="float-right text-sm text-warning"><i
-                                                        class="fas fa-star"></i></span>
-                                                    </h3>
-                                                    <p class="text-sm">The subject goes here</p>
-                                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                                </div>
-                                            </div>
-                                            <!-- Message End -->
-                                        </a>
-                                        <div class="dropdown-divider"></div>
-                                        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                                    </div>
-                                </li>
-                                <!-- Notifications Dropdown Menu -->
-                                <li class="nav-item dropdown">
-                                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                                        <span class="dropdown-item dropdown-header">15 Notifications</span>
-                                        <div class="dropdown-divider"></div>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                                            <span class="float-right text-muted text-sm">3 mins</span>
-                                        </a>
-                                        <div class="dropdown-divider"></div>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-users mr-2"></i> 8 friend requests
-                                            <span class="float-right text-muted text-sm">12 hours</span>
-                                        </a>
-                                        <div class="dropdown-divider"></div>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-file mr-2"></i> 3 new reports
-                                            <span class="float-right text-muted text-sm">2 days</span>
-                                        </a>
-                                        <div class="dropdown-divider"></div>
-                                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                                    </div>
-                                </li>
                                 <li class="nav-item">
                                     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                                         <i class="fas fa-expand-arrows-alt"></i>
@@ -313,9 +231,6 @@
                 <script src="{{ asset('') }}assets/plugins/chart.js/Chart.min.js"></script>
                 <!-- Sparkline -->
                 <script src="{{ asset('') }}assets/plugins/sparklines/sparkline.js"></script>
-                <!-- JQVMap -->
-                <script src="{{ asset('') }}assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-                <script src="{{ asset('') }}assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
                 <!-- jQuery Knob Chart -->
                 <script src="{{ asset('') }}assets/plugins/jquery-knob/jquery.knob.min.js"></script>
                 <!-- daterangepicker -->
@@ -323,8 +238,6 @@
                 <script src="{{ asset('') }}assets/plugins/daterangepicker/daterangepicker.js"></script>
                 <!-- Tempusdominus Bootstrap 4 -->
                 <script src="{{ asset('') }}assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-                <!-- Summernote -->
-                <script src="{{ asset('') }}assets/plugins/summernote/summernote-bs4.min.js"></script>
                 <!-- overlayScrollbars -->
                 <script src="{{ asset('') }}assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
                 <!-- AdminLTE App -->
