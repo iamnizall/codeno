13d640de30231dd5f33d89dd22838a133ef7b3fa
iamnizall <nizalrizqi69@gmail.com>
2022-01-10 11:37:27 +0700

    delete remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,16 +55,7 @@
                             </span>
                             @enderror
                         </div>
-                        <div class="form-group">
-                           <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div>
-                    </div>
-
+                        
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">
                             {{ __('Login') }}
